cb00d9a917de4541037cb636b8187b448550f5b6
bulgokik <ns.nbnb77777@gmail.com>
2026-07-16 16:43:17 +0000

    compile: change xxKernelSU

--- compile-lv.sh
+++ compile-lv.sh
@@ -3,7 +3,7 @@
 # Many parts of this script were taken from @REIGNZ, @idkwhoiam322 and @raphielscape . Huge thanks to them.
 
 # KernelSu
-curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s legacy
+curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/kernel/setup.sh" | bash -s 
 #Cleaning
 rm -rf out
 make clean
