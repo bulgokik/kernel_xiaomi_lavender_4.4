1434b313b668c9113f03f6951d9e5b5c2240fac4
bulgokik <ns.nbnb77777@gmail.com>
2026-07-17 01:13:24 +0700

    Fix URL in KernelSu setup script

--- compile-lv.sh
+++ compile-lv.sh
@@ -3,7 +3,7 @@
 # Many parts of this script were taken from @REIGNZ, @idkwhoiam322 and @raphielscape . Huge thanks to them.
 
 # KernelSu
-curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/kernel/setup.sh" | bash -s master
+curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/master/kernel/setup.sh" | bash -s 
 #Cleaning
 rm -rf out
 make clean
